d145761a3896719643d26809478e2201064940fb
romes <rodrigo.m.mesquita@gmail.com>
2021-08-31 10:41:58 +0100

    hello, kernel world!

--- build-grub.sh
+++ build-grub.sh
@@ -2,12 +2,24 @@
 
 source env.sh
 
-cd "$ROOT/other"
+# TODO: Add curl + tar of source
 
-cd objconv/source
+cd "$ROOT/other/objconv/source"
 ./build.sh
-ln objconv "$ROOT/opt/bin"
+ln objconv "$ROOT/opt/other/bin/objconv"
+
+cd "$ROOT/other/xorriso-1.5.4"
+./configure --prefix="$ROOT/opt/other"
+make
+make install
 
 cd "$ROOT/other/grub-2.06"
-./configure --prefix="$PREFIX"
+./autogen.sh
+cd "$ROOT/other"
+mkdir -p "build-grub"
+cd build-grub
+../grub-2.06/configure --prefix="$ROOT/opt/other" --disable-werror TARGET_CC=i686-elf-gcc TARGET_OBJCOPY=i686-elf-objcopy TARGET_STRIP=i686-elf-strip TARGET_NM=i686-elf-nm TARGET_RANLIB=i686-elf-ranlib --target=i686-elf
+make
 make install
+
+cd "$ROOT"
--- env.sh
+++ env.sh
@@ -4,4 +4,4 @@ export ROOT="$(pwd)"
 
 export PREFIX="$ROOT/opt/cross"
 export TARGET=i686-elf
-export PATH="$PREFIX/bin:$ROOT/opt/bin:$PATH"
+export PATH="$ROOT/opt/other/bin:$PREFIX/bin:$PATH"
